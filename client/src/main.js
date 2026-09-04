5cf69bbe83790d16b3a83cb76da78402b8472e63
Faketerminator1779 <faketerminator1779@gmail.com>
2026-09-04 21:28:19 +0200

    client fix

--- client/src/main.js
+++ client/src/main.js
@@ -6,56 +6,61 @@ import { createGameScene } from './scenes/GameScene.js';
 const BASE_WIDTH = 240;
 const BASE_HEIGHT = 160;
 
-const app = new PIXI.Application();
+async function main() {
+    const app = new PIXI.Application();
 
-await app.init({
-    width: BASE_WIDTH,
-    height: BASE_HEIGHT,
-    background: '#696969'
-});
+    await app.init({
+        width: BASE_WIDTH,
+        height: BASE_HEIGHT,
+        background: '#696969'
+    });
 
-document.body.appendChild(app.canvas);
+    document.body.appendChild(app.canvas);
 
-await PIXI.Assets.load([
-    '/assets/numbers.json', 
-    '/assets/pawn.png'
-])
+    await PIXI.Assets.load([
+        '/assets/numbers.json', 
+        '/assets/pawn.png'
+    ])
+    
 
-function resize() {
-    const scale = Math.floor(
-        Math.min(
-            window.visualViewport.width / BASE_WIDTH,
-            window.visualViewport.height / BASE_HEIGHT
-        )
-    )  
-    app.canvas.style.width = `${BASE_WIDTH * Math.max(scale, 1)}px`
-    app.canvas.style.height = `${BASE_HEIGHT * Math.max(scale, 1)}px`
+    function resize() {
+        const scale = Math.floor(
+            Math.min(
+                window.visualViewport.width / BASE_WIDTH,
+                window.visualViewport.height / BASE_HEIGHT
+            )
+        )  
+        app.canvas.style.width = `${BASE_WIDTH * Math.max(scale, 1)}px`
+        app.canvas.style.height = `${BASE_HEIGHT * Math.max(scale, 1)}px`
+    }
+
+    window.addEventListener('resize', resize);
+    resize();
+
+    let currentScene;
+
+    function setScene(id) {
+
+        if (currentScene) {
+        if (currentScene?.cleanup) {
+            currentScene.cleanup();
+        }
+        currentScene.destroy({ children: true });
+        app.stage.removeChild(currentScene);
+        }
+
+        if (id === 1) {
+            currentScene = createStartScene(app, setScene);
+        }
+
+        if (id === 2) {
+            currentScene = createGameScene(app);
+        }
+
+        app.stage.addChild(currentScene);
+    }
+
+    setScene(1);
 }
 
-window.addEventListener('resize', resize);
-resize();
-
-let currentScene;
-
-function setScene(id) {
-
-    if (currentScene) {
-      if (currentScene?.cleanup) {
-        currentScene.cleanup();
-      }
-      currentScene.destroy({ children: true });
-      app.stage.removeChild(currentScene);
-    }
-
-    if (id === 1) {
-        currentScene = createStartScene(app, setScene);
-    }
-
-    if (id === 2) {
-        currentScene = createGameScene(app);
-    }
-
-    app.stage.addChild(currentScene);
-}
-
-setScene(1);
+main()
--- client/src/network/socket.js
+++ client/src/network/socket.js
@@ -1,3 +1,7 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:3000');
+const serverUrl = import.meta.env.DEV
+    ? 'http://localhost:3000'
+    : 'https://wwb-5kde.onrender.com';
+
+export const socket = io(serverUrl);
